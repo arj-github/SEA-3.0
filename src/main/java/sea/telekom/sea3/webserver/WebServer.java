096db0c85571c7730f99c090a120260e96f777bb
arj <a.rj@gmx.de>
2021-06-09 11:27:13 +0200

    Spring Boot WebServer aufgesetzt

---
+++ src/main/java/sea/telekom/sea3/webserver/WebServer.java
@@ -0,0 +1,12 @@
+
+package sea.telekom.sea3.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServer /* implements SpringBootApplication */ {
+	public static void main(String[] args) {
+		SpringApplication.run(WebServer.class, args);
+	}
+}
