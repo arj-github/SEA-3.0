ad643d8253da73455969a3bf214736bae5568178
arj <a.rj@gmx.de>
2021-06-10 12:21:10 +0200

    aufgeraumt

---
+++ src/main/resources/public/javaScript/myscript.js
@@ -0,0 +1,7 @@
+var para = document.getElementById("id4711");
+para.textContent = "Hello from JavaScript";
+  fetch("http://localhost:8080/personen.json")
+ 	
+// 	fetch("personen.json")
+ 		.then( irgendwas => irgendwas.json() )
+ 		.then(myjson => console.log(myjson.Personen[0])); 
