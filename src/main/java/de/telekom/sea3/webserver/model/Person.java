0eca06d0480ca425188153ae39f966a7334929f7
arj <a.rj@gmx.de>
2021-06-23 14:18:06 +0200

    Update geht

--- src/main/java/de/telekom/sea3/webserver/model/Person.java
+++ src/main/java/de/telekom/sea3/webserver/model/Person.java
@@ -1,23 +1,28 @@
 package de.telekom.sea3.webserver.model;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Version;
 
-@Table(name="personen")
+@Table(name = "personen")
 @Entity
 public class Person {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column
 	private String vorname;
 	@Column
 	private String nachname;
+	@Column(name="geb_datum")
+	private LocalDate gebDatum; //YYYYMMDD
 	@Column
 	private String anrede;
 	@Column
@@ -25,14 +30,17 @@ public class Person {
 	@Column
 	private String strasse;
 	@Column
-	private	String ort;
+	private String ort;
 	@Column
 	private String plz;
+	@Version
+	private Long version;
 
-	
-	public Person() {};
-	public Person(int i, String anrede, String vorname, String nachname,
-			String email, String strasse, String plz, String ort) {
+	public Person() {
+	};
+
+	public Person(int i, String anrede, String vorname, String nachname, String email, String strasse, String plz,
+			String ort, LocalDate gebDatum) {
 		this.id = i;
 		this.anrede = anrede;
 		this.vorname = vorname;
@@ -41,6 +49,7 @@ public class Person {
 		this.strasse = strasse;
 		this.plz = plz;
 		this.ort = ort;
+		this.gebDatum = gebDatum;
 
 	}
 
@@ -76,30 +85,53 @@ public class Person {
 		this.id = id;
 
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getStrasse() {
 		return strasse;
 	}
+
 	public void setStrasse(String strasse) {
 		this.strasse = strasse;
 	}
+
 	public String getOrt() {
 		return ort;
 	}
+
 	public void setOrt(String ort) {
 		this.ort = ort;
 	}
-	public String getPLZ() {
+
+	public String getPlz() {
 		return plz;
 	}
-	public void setPLZ(String plz) {
+
+	public void setPlz(String plz) {
 		this.plz = plz;
+	}
+
+	public Long getVersion() {
+		return version;
+	}
+
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+	public LocalDate getGebDatum() {
+		return gebDatum;
+	}
+
+	public void setGebDatum(LocalDate gebDatum) {
+		this.gebDatum = gebDatum;
 	}
 
 }
--- src/main/java/de/telekom/sea3/webserver/service/PersonService.java
+++ src/main/java/de/telekom/sea3/webserver/service/PersonService.java
@@ -1,5 +1,7 @@
 package de.telekom.sea3.webserver.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import de.telekom.sea3.webserver.model.*;
 import de.telekom.sea3.webserver.repo.PersonRepository;
-
 
 @Service
 public class PersonService {
@@ -23,34 +24,33 @@ public class PersonService {
 		this.personRepository = personRepository;
 	}
 
-
 	public Size getSize() {
 		return new Size(personRepository.count());
 	}
-	
+
 	public Person get(long id) {
-		Optional<Person> op= personRepository.findById((Long) id);
+		Optional<Person> op = personRepository.findById((Long) id);
 		Person person = op.get();
-		
+
 		if (person.equals(null)) {
 			throw new NullPointerException();
-		} else return person;
+		} else
+			return person;
 	}
-	
+
 	public long count() {
 		return personRepository.count();
-		
+
 	}
 
 	// ArrayList personen aufbauen
 	public Personen getAllPersons() {
 		Personen ps = new Personen();
-			for (Person p:personRepository.findAll()) {
-				ps.getPersonen().add(p);
-			}
-			return ps;
+		for (Person p : personRepository.findAll()) {
+			ps.getPersonen().add(p);
+		}
+		return ps;
 	}
-	
 
 	public Person add(Person person) {
 		personRepository.save(person);
@@ -58,65 +58,95 @@ public class PersonService {
 	}
 
 	public void delete(long id) {
-		Optional<Person> op = personRepository.findById((Long) id );
+		Optional<Person> op = personRepository.findById((Long) id);
 		Person person = op.get();
-		
+
 		if (person.equals(null)) {
 			throw new NullPointerException();
-		} else personRepository.delete(person);
+		} else
+			personRepository.delete(person);
 	}
-	
+
 	public void update(Person person) {
+
+		var anrede = person.getAnrede();
+		var vorname = person.getVorname();
+		var nachname = person.getNachname();
+		LocalDate gebDatum = person.getGebDatum();
+		var email = person.getEmail();
+		var str = person.getStrasse();
+		var plz = person.getPlz();
+		var ort = person.getOrt();
+		var version = person.getVersion();
+
+		System.out.println(anrede);
+		System.out.println(vorname);
+		System.out.println(nachname);
+		DateTimeFormatter formGebDate = DateTimeFormatter.ofPattern("ddLLLLyyyy");
+		String gebDateString = gebDatum.format(formGebDate);
+		System.out.println(gebDateString);
+		System.out.println(email);
+		System.out.println(str);
+		System.out.println(plz);
+		System.out.println(ort);		
+		System.out.println(version);
 		
-		var anrede=person.getAnrede();
-		var vorname=person.getVorname();
-		var nachname=person.getNachname();
-		var email=person.getEmail();
-		var str=person.getStrasse();
-		var plz=person.getPLZ();
-		var ort=person.getOrt();
-		
-		Optional<Person> op = personRepository.findById((Long) person.getId() );
+
+		Optional<Person> op = personRepository.findById((Long) person.getId());
 		Person personToBeSaved = op.get();
-		
+
+		System.out.println("PersonToBeSaved: " + personToBeSaved);
+
 		if (personToBeSaved.equals(null)) {
 			throw new NullPointerException();
-		} else
-			if (!anrede.equals("")){
-				personToBeSaved.setAnrede(anrede);
-			};
-			if (!vorname.equals("")){
-				System.out.println("Hallo");
-				personToBeSaved.setVorname(vorname);
-			};
-			if (!nachname.equals("")){
-				personToBeSaved.setNachname(nachname);
-			};
-			if (!email.equals("")){
-				personToBeSaved.setEmail(email);
-			};
-			if (!str.equals("")){
-				personToBeSaved.setStrasse(str);
-			};
-			if (!plz.equals("")){
-				personToBeSaved.setPLZ(plz);
-			};
-			if (!ort.equals("")){
-				personToBeSaved.setOrt(ort);
-			};
-			personRepository.save(personToBeSaved);
-		
+		} else if (!anrede.equals("")) {
+			personToBeSaved.setAnrede(anrede);
 		}
+		
+		if (!vorname.equals("")) {
+			System.out.println("Hallo");
+			personToBeSaved.setVorname(vorname);
+		}
+
+		if (!nachname.equals("")) {
+			personToBeSaved.setNachname(nachname);
+		}
+
+		if (!email.equals("")) {
+			personToBeSaved.setEmail(email);
+		}
+
+		if (!str.equals("")) {
+			personToBeSaved.setStrasse(str);
+		}
+		if (!plz.equals("")) {
+			personToBeSaved.setPlz(plz);
+		}
+
+		if (!ort.equals("")) {
+			personToBeSaved.setOrt(ort);
+		}
+
+		if (!gebDatum.equals(null)) {
+			personToBeSaved.setGebDatum(gebDatum);
+		}
+			personToBeSaved.setVersion(version);
+
+
+		personRepository.save(personToBeSaved);
+
+	}
 
 	public Personen getPersonById(long id) {
 		Personen ps = new Personen();
-		Optional<Person> op = personRepository.findById((Long) id); 
-		
+		Optional<Person> op = personRepository.findById((Long) id);
+
 		if (!op.equals(null)) {
 			Person personFound = op.get();
 			ps.getPersonen().add(personFound);
 			return ps;
-		} throw new NullPointerException();
+		}
+		throw new NullPointerException();
 	}
-	
+
 }
