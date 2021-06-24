efe1be1c6da6311d36e99f2de2197bada064d80c
arj <a.rj@gmx.de>
2021-06-24 15:24:25 +0200

    At_Query eingebaut

--- src/main/java/de/telekom/sea3/webserver/repo/PersonRepository.java
+++ src/main/java/de/telekom/sea3/webserver/repo/PersonRepository.java
@@ -1,11 +1,17 @@
 package de.telekom.sea3.webserver.repo;
 
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import de.telekom.sea3.webserver.model.Person;
 
 @Repository
 public interface PersonRepository extends CrudRepository<Person,Long>{
+	
+	@Query(value="SELECT * from personen where ort=:ort",nativeQuery=true )
+	Iterable<Person> selectPersonen(@Param("ort") String ort);
 
 }
